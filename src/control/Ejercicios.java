9492ba58b5ece90f8b394d872108092bfdb62693
mauricio <mauricio@Profesor>
2019-01-29 10:04:26 +0000

    Se obtiene la lista de victorias,empates y derrotas por cada equipo.

--- src/control/Ejercicios.java
+++ src/control/Ejercicios.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
@@ -15,13 +15,64 @@ import modelo.Estudiante;
 import modelo.Persona;
 
 public class Ejercicios {
-	// 24 enero 2019
+	// 29 enero 2019
 	public HashMap<String, ArrayList<Integer>> resultadosEquipos(String rutaPartidos)
 	// devuelve un mapa de equipos
 	// por cada equipo hay una lista de contadores
 	// que representan VICTORIAS, EMPATES Y DERROTAS
 	{
+		try {
+			BufferedReader fichero;
+			fichero = new BufferedReader(new FileReader(rutaPartidos));
+			String registro;
+			HashMap<String, ArrayList<Integer>> equipos = new HashMap<String, ArrayList<Integer>>();
+			while ((registro = fichero.readLine()) != null) {
+				String[] campos = registro.split("#");
+				if (campos[3].equals("")) // ultimo partido jugado..
+					break;
+				String eL = campos[2];
+				String gL = campos[3];
+				String eV = campos[4];
+				String gV = campos[5];
+
+				// gracias Byron..!!
+				// si no existe eL, eV lo añadimos al mapa..
+
+				if (!equipos.containsKey(eL))
+					equipos.put(eL, new ArrayList<Integer>(Arrays.asList(0, 0, 0)));
+
+				if (!equipos.containsKey(eV))
+					equipos.put(eV, new ArrayList<Integer>(Arrays.asList(0, 0, 0)));
+
+				// cual fue el resultado ..?
+
+				if (gL.compareTo(gV) > 0) {// gana Local
+					equipos.get(eL).set(0, equipos.get(eL).get(0) + 1);
+					equipos.get(eV).set(2, equipos.get(eV).get(2) + 1);
+
+				} else if (gL.compareTo(gV) < 0) // gana Visitante
+				{// gana Local
+					equipos.get(eL).set(2, equipos.get(eL).get(2) + 1);
+					equipos.get(eV).set(0, equipos.get(eV).get(0) + 1);
+				} else { // empate
+
+					equipos.get(eL).set(1, equipos.get(eL).get(1) + 1);
+					equipos.get(eV).set(1, equipos.get(eV).get(1) + 1);
+				}
+
+			}
+			fichero.close();
+			System.out.println("Fin de la lectura del fichero");
+			return equipos;
+
+		} catch (FileNotFoundException excepcion) {
+			System.out.println("fichero no encontrado");
+
+		} catch (IOException e) {
+			System.out.println("IO Excepcion");
+		}
 		return null;
+
 	}
 
 	public void mostrarNumeroPartidosJugadosTry(String rutaPartidos) {
@@ -834,13 +885,14 @@ public class Ejercicios {
 		// ejercicios.creaListaPersonas("ficheros/personas.txt", "##");
 		// HashMap<String, Integer> equipos =
 		// HashMap<String, Integer> x =
-		HashMap<String, Integer> x = ejercicios.comprobarPartidos("ficheros/partidos.txt");
+		// HashMap<String, Integer> x =
+		// ejercicios.comprobarPartidos("ficheros/partidos.txt");
 		// ArrayList<Equipo> equipos =
 		// ArrayList<Equipo> x = ejercicios.crearListaEquipos("ficheros/equipos.txt");
 		// HashMap<String, Equipo> equipos =
 		// ejercicios.crearMapaEquipos("ficheros/equipos.txt");
 		// ejercicios.mostrarNumeroPartidosJugadosTry("ficheros/partidos.txt");
-
+		HashMap<String, ArrayList<Integer>> x = ejercicios.resultadosEquipos("ficheros/partidos.txt");
 		System.exit(0); // finaliza el programa ...
 
 		int[] lista1 = { 2, 4, 8, 9 };
